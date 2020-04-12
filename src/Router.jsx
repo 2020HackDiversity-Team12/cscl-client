f9d204efa0b03712c6e889a58c13380bcc8ba108
Krishna Farvil <fkrishna.dev@gmail.com>
2020-04-12 06:28:15 -0400

    Fix typo

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 // import components
 import App from "./App/App";
-import HomePage from "./pages/HomePage";
+import SharedComponents from "./pages/SharedComponents";
 
 function Router() {
   return (
     <BrowserRouter>
       <App>
         <Switch>
-          <Route path="/" component={HomePage} />
+          <Route path="/components" component={SharedComponents} />
         </Switch>
       </App>
     </BrowserRouter>
